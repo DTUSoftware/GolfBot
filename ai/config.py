390d14cc27f03dc8741cdfd7a3170ea16e560891
HeroGamers <hero@herogamers.dev>
2023-04-19 10:31:42 +0200

    Add main code

---
+++ ai/config.py
@@ -0,0 +1,5 @@
+CURRENT_MODEL = "models/best.onnx"
+PRETRAINED_MODEL = "yolov8n.pt"
+DATA = "data/data.yaml"
+EPOCHS = 3
+imgsz = 640 # needs to be a multiple of 32
---
+++ ai/main.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+import config
+
+## Lots of example code and documentation from 
+## https://github.com/ultralytics/ultralytics
+
+# Load a model
+## pretrained models (small/fastest -> largest/slowest):
+## YOLOv8n	YOLOv8s	YOLOv8m	YOLOv8l	YOLOv8x
+model = YOLO(config.PRETRAINED_MODEL)  # load a pretrained model (recommended for training)
+
+# Use the model
+model.train(data=config.DATA, epochs=config.EPOCHS)  # train the model
+# metrics = model.val()  # evaluate model performance on the validation set
+success = model.export(format="onnx")  # export the model to ONNX format
