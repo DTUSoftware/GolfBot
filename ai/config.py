883f11974cfc63821aab14e71d1c657daf023049
OxTheLade <59265003+OxTheLade@users.noreply.github.com>
2023-04-26 10:18:03 +0200

    Added model_testing for testing and running the model with webcam. Updated the config and requirement too. Two models have been uploaded too, but only trained on low epochs. datasets folder structure has been updated too.

--- ai/config.py
+++ ai/config.py
@@ -1,5 +1,5 @@
-CURRENT_MODEL = "models/best.onnx"
+CURRENT_MODEL = "models/26041010"
 PRETRAINED_MODEL = "yolov8n.pt"
-DATA = "data/data.yaml"
+DATA = "datasets/RoboFlow1904/data.yaml"
 EPOCHS = 3
 imgsz = 640 # needs to be a multiple of 32
