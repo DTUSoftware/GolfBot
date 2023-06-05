8a40ad97c1070533be22e9b75c474c52f637978e
HeroGamers <hero@herogamers.dev>
2023-06-06 00:04:24 +0200

    Add code for saving custom datasets

--- client/ai/model_testing.py
+++ client/ai/model_testing.py
@@ -1,48 +1,85 @@
 import cv2
 import os
 import torch
+from datetime import datetime
+from PIL import Image
 from ultralytics import YOLO
 from ultralytics.yolo.utils.plotting import Annotator
 
 VIDEO_INPUT = int(os.environ.get('VIDEO_INPUT', 0))
 CURRENT_MODEL = os.environ.get("CURRENT_MODEL", "models/20230605_nico1")
+DATA = os.environ.get("DATA", "datasets/RoboFlow0506-1")
 
-# Set device for AI processing
-torchDevice = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-device = 0 if "cuda" in torchDevice.type else "cpu"
-print(f"Using device: {device} ({torchDevice.type}: index {torchDevice.index})")
 
-# Load the model from the local .pt file
-model = YOLO(CURRENT_MODEL + ".pt")
+def save_result(results):
+    print("Saving result...")
+    if not os.path.exists(DATA + "/custom/images"):
+        os.makedirs(DATA + "/custom/images", exist_ok=True)
+    if not os.path.exists(DATA + "/custom/labels"):
+        os.makedirs(DATA + "/custom/labels", exist_ok=True)
 
-# Open a connection to the webcam
-cap = cv2.VideoCapture(VIDEO_INPUT)
+    for result in results:
+        image_name: str = datetime.now().strftime("CustomImage-%Y-%m-%d_%H%M%S")
+        image = Image.fromarray(result.orig_img)
+        if not image:
+            continue
+        image.save(f"{DATA}/custom/images/{image_name}.jpg")
+        print(f"Saved image as {image_name}.jpg!")
 
-while True:
-    # Capture a frame from the webcam
-    ret, frame = cap.read()
+        labels = []
+        for box in result.boxes:
+            labels.append(box.cls + " " + " ".join(box.xywhn) + "\n")
+        if not labels:
+            continue
+        with open(f"{DATA}/custom/labels/{image_name}.txt", "w+") as file:
+            file.write("\n".join(labels))
+            print(f"Saved image labels as {image_name}.txt!")
 
-    # Send the frame to the model for prediction
-    results = model.predict(frame, device=device)
 
-    # Draw bounding boxes and labels on the image
-    annotator = Annotator(frame)
-    for r in results:
-        boxes = r.boxes
-        for box in boxes:
-            b = box.xyxy[0]  # get box coordinates in (top, left, bottom, right) format
-            c = box.cls
-            conf = box.conf.item()
-            annotator.box_label(b, model.names[int(c)])
-            print(f"Class: {model.names[int(c)]}, Confidence: {conf:.2f}, x: {b[0]}, y: {b[1]}")
-        frame = annotator.result()
+def test_model():
+    # Set device for AI processing
+    torch_device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    device = 0 if "cuda" in torch_device.type else "cpu"
+    print(f"Using device: {device} ({torch_device.type}: index {torch_device.index})")
 
-    cv2.imshow('Camera Feed', frame)
+    # Load the model from the local .pt file
+    model = YOLO(CURRENT_MODEL + ".pt")
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # Open a connection to the webcam
+    cap = cv2.VideoCapture(VIDEO_INPUT)
 
-# Release the webcam when done
-cap.release()
-# Destroy
-cv2.destroyAllWindows()
+    while True:
+        # Capture a frame from the webcam
+        ret, frame = cap.read()
+
+        # Send the frame to the model for prediction
+        results = model.predict(frame, device=device)
+
+        # Draw bounding boxes and labels on the image
+        annotator = Annotator(frame)
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                b = box.xyxy[0]  # get box coordinates in (top, left, bottom, right) format
+                c = box.cls
+                conf = box.conf.item()
+                annotator.box_label(b, model.names[int(c)])
+                print(f"Class: {model.names[int(c)]}, Confidence: {conf:.2f}, x: {b[0]}, y: {b[1]}")
+            frame = annotator.result()
+
+        cv2.imshow('Camera Feed', frame)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            break
+        elif key == ord('s'):
+            save_result(results)
+
+    # Release the webcam when done
+    cap.release()
+    # Destroy
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    test_model()
--- client/ai/train.py
+++ client/ai/train.py
@@ -2,18 +2,17 @@ from ultralytics import YOLO
 import torch
 import os
 
-#VIDEO_INPUT = int(os.environ.get('VIDEO_INPUT', 1))
-#CURRENT_MODEL = os.environ.get("CURRENT_MODEL", "models/20230601")
 PRETRAINED_MODEL = os.environ.get("PRETRAINED_MODEL", "yolov8n.pt")
-DATA = os.environ.get("DATA", "datasets/RoboFlow0506-1/data.yaml")
+DATA = os.environ.get("DATA", "datasets/RoboFlow0506-1")
 EPOCHS = int(os.environ.get("EPOCHS", 300))
 IMGSZ = int(os.environ.get("IMGSZ", 640))  # needs to be a multiple of 32
 
+
 def train():
     # Set device for AI processing
-    torchDevice = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    device = 0 if "cuda" in torchDevice.type else "cpu"
-    print(f"Using device: {device} ({torchDevice.type}: index {torchDevice.index})")
+    torch_device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    device = 0 if "cuda" in torch_device.type else "cpu"
+    print(f"Using device: {device} ({torch_device.type}: index {torch_device.index})")
 
     ## Lots of example code and documentation from
     ## https://github.com/ultralytics/ultralytics
@@ -24,7 +23,7 @@ def train():
     model = YOLO(PRETRAINED_MODEL)  # load a pretrained model (recommended for training)
 
     # Use the model
-    model.train(data=DATA, epochs=EPOCHS, device=device)  # train the model
+    model.train(data=DATA + "/data.yaml", epochs=EPOCHS, device=device)  # train the model
     # metrics = model.val()  # evaluate model performance on the validation set
     success = model.export(format="onnx")  # export the model to ONNX format
     # success = model.export(format="pt")  # export the model to pt format too
