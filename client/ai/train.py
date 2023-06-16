6ab00c118cd05bbe36385f1f4f42dba40f5f4a4c
NicolaiUdbye <70693777+NicolaiUdbye@users.noreply.github.com>
2023-06-16 15:42:34 +0200

    new front

--- client/ai/train.py
+++ client/ai/train.py
@@ -6,7 +6,7 @@ from ultralytics import YOLO
 # Path to the pretrained model
 PRETRAINED_MODEL = os.environ.get("PRETRAINED_MODEL", "yolov8n.pt")
 # Path to the dataset
-DATA = os.environ.get("DATA", "datasets/RoboFlow1606-deepfryedition")
+DATA = os.environ.get("DATA", "datasets/RoboFlow1606-newfront")
 # Number of epochs to train for
 EPOCHS = int(os.environ.get("EPOCHS", 1000))
 # Image size
