d3552cee42da60501c63a5988f9cab27174ca4dc
HeroGamers <hero@herogamers.dev>
2023-06-20 17:36:56 +0200

    Change delivery time to 6 minutes

--- client/app/core/robot_racing.py
+++ client/app/core/robot_racing.py
@@ -123,7 +123,7 @@ async def do_race_iteration(track: path_algorithm.Track, ai_queue: multiprocessi
 
         objects_to_navigate_to: List[List[Tuple[int, int]]] = []
         if track.balls and (
-                len(seen_ball_queue) < 10 or len([seen_ball for seen_ball in seen_ball_queue if seen_ball]) >= 4) and time_taken <= 7 * 60:
+                len(seen_ball_queue) < 10 or len([seen_ball for seen_ball in seen_ball_queue if seen_ball]) >= 4) and time_taken <= 6 * 60:
             # Get every ball that's not golden
             objects_to_navigate_to = [ball.drivePath for ball in track.balls if
                                       not ball.golden and ball.get_drive_path()]
